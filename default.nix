b00998b256303210a4fb8b5a54ca94647f5abc27
pveber <philippe.veber@gmail.com>
2016-03-21 18:53:28 +0100

    added a perl CPAN package

--- default.nix
+++ default.nix
@@ -1,8 +1,19 @@
-{ nixpkgs ? import <nixpkgs> {} }:
+{ system ? import <nixpkgs> {} }:
 
-let callPackage = nixpkgs.callPackage; in
+with system ; {
 
-{
+  FileCopyLink = buildPerlPackage rec {
+    name = "File-Copy-Link-0.140";
+    src = fetchurl {
+      url = "mirror://cpan/authors/id/R/RM/RMBARKER/${name}.tar.gz";
+      sha256 = "2063656dcd38bade43dc7f1e2ef5f1b6a8086c2f15d37b334189bd2a28e8ffeb";
+    };
+    meta = {
+      description = "Extension for replacing a link by a copy of the linked file";
+      license = with stdenv.lib.licenses; [ artistic1 gpl1Plus ];
+    };
+  };
+
   art = callPackage pkgs/art { };
 
   cisa = callPackage pkgs/cisa { };
