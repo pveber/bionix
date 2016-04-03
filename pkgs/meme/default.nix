43c83b10199a7398cc5707be8b284a68dbaf9b9c
pveber <philippe.veber@gmail.com>
2016-04-03 23:33:07 +0200

    meme: fixed perl paths

--- pkgs/meme/default.nix
+++ pkgs/meme/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, perl, zlib}:
+{stdenv, fetchurl, perl, perlPackages, zlib}:
 
 stdenv.mkDerivation {
   name = "meme-4.11.1";
@@ -7,9 +7,15 @@ stdenv.mkDerivation {
     sha256 = "120ha97nbdgazvziixibc2xjz73222vja424kz2j512wn92j0q32";
   };
 
-  buildInputs = [perl zlib];
+  buildInputs = [perl perlPackages.XMLParser perlPackages.XMLSimple zlib];
 
   configureFlags = ''--with-url="http://meme-suite.org" --enable-build-libxml2 --enable-build-libxslt'';
+
+  postFixup = ''
+    substituteInPlace $out/bin/meme-chip --replace "${perl}/bin/perl" \
+        "${perl}/bin/perl -I${perlPackages.XMLSimple}/${perl.libPrefix} \
+                          -I${perlPackages.XMLParser}/${perl.libPrefix}"
+  '';
 
   meta = {
     description = "Motif-based sequence analysis tools";
